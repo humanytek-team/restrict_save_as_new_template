3d63fd92068ece9542b4f61bd32727cb76cbb56d
Moises Navarro <moisalejandro@gmail.com>
2022-03-08 11:05:45 -0600

    [INIT]

---
+++ README.rst
@@ -0,0 +1,3 @@
+Restrict Save as New Template
+=============================
+Restrict the use of the button `SAVE AS NEW TEMPLATE` inside the mail composition. Only usable by group administrator.
---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Restrict Save as New Template",
+    "version": "13.0.0.1.0",
+    "author": "HomebrewSoft",
+    "website": "https://homebrewsoft.dev",
+    "license": "LGPL-3",
+    "depends": [
+        "mail",
+    ],
+    "data": [
+        # security
+        # data
+        # reports
+        # views
+        "views/mail_compose_message.xml",
+    ],
+}
